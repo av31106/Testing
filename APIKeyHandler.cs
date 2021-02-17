9f5c3dd052a9c2ce99ef6df3b9293b5a487ba886
av31106 <61911177+av31106@users.noreply.github.com>
2021-02-17 21:58:50 +0530

    API Key

---
+++ APIKeyHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+public class APIKeyHandler : DelegatingHandler
+{
+    private string AllApiKeys = ConfigurationManager.AppSettings["ApiKeys"];
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        //this is for only gethash code.
+        if (request.RequestUri.ToString().Contains("CalculateHash/GetHash") ||
+            request.RequestUri.ToString().Contains("CalculateHash/GetHashIos"))
+        {
+            //Allow the request to process further down the pipeline.
+            var responseTemp = await base.SendAsync(request, cancellationToken);
+
+            //Return the response back up the chain.
+            return responseTemp;
+        }
+
+        bool isValidAPIKey = false;
+        IEnumerable<string> lsHeaders;
+        //Validate that the api key exists.
+        var checkApiKeyExists = request.Headers.TryGetValues("Api-Key", out lsHeaders);
+        if (!string.IsNullOrEmpty(AllApiKeys) && checkApiKeyExists)
+        {
+            string apiKey = lsHeaders.FirstOrDefault();
+            string resultKey = AllApiKeys.Split(',').Where(col => col == apiKey).FirstOrDefault();
+            if (resultKey != null && resultKey == apiKey)
+                isValidAPIKey = true;
+        }
+
+        //If the key is not valid, return an http status code.
+        if (!isValidAPIKey)
+            return request.CreateResponse(HttpStatusCode.Forbidden, "Invalid Api Key.");
+
+        //Allow the request to process further down the pipeline.
+        var response = await base.SendAsync(request, cancellationToken);
+
+        //Return the response back up the chain.
+        return response;
+    }
+}
---
+++ AuthorizationAttribute.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Configuration;
+using System.Net.Http;
+using System.Net;
+
+public class NGAuthorizationAttribute : ActionFilterAttribute, IActionFilter
+{
+    private string AllUserIds = ConfigurationManager.AppSettings["UserIds"];
+    private string AllUserPwds = ConfigurationManager.AppSettings["UserPwds"];
+
+    public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+    {
+        IEnumerable<string> lsHeaders;
+        try
+        {
+            var checkUserExists = actionContext.Request.Headers.TryGetValues("Authorization", out lsHeaders);
+            if (!string.IsNullOrEmpty(AllUserIds) && !string.IsNullOrEmpty(AllUserPwds) && checkUserExists)
+            {
+                CMSSanaApi.Models.ICryptography crypt = new CMSSanaApi.Models.Cryptography();
+                string cipherTestTemp, idAndPwd = "";
+                string authString = lsHeaders.FirstOrDefault();
+                string algo = authString.Substring(0, authString.IndexOf(' '));
+                if (!string.IsNullOrEmpty(algo) && algo.ToUpper() == "AES")
+                {
+                    cipherTestTemp = authString.Substring(authString.IndexOf(' ') + 1, authString.Length - (algo.Length + 1));
+                    idAndPwd = crypt.AesDecrypt(cipherTestTemp);
+                }
+                else if (!string.IsNullOrEmpty(algo) && algo.ToUpper() == "BASIC")
+                {
+                    cipherTestTemp = authString.Substring(authString.IndexOf(' ') + 1, authString.Length - (algo.Length + 1));
+                    var temp = Convert.FromBase64String(cipherTestTemp);
+                    idAndPwd = System.Text.Encoding.UTF8.GetString(temp);
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.BadRequest, "Incorrect Authorization Algorithm.");
+                }
+
+                if (idAndPwd.Split(':').Length == 2)
+                {
+                    string resultUserId = AllUserIds.Split(',').Where(col => col == idAndPwd.Split(':')[0]).FirstOrDefault();
+                    string resultPwd = AllUserPwds.Split(',').Where(col => col == idAndPwd.Split(':')[1]).FirstOrDefault();
+                    if (resultUserId != null && resultPwd != null && resultUserId == idAndPwd.Split(':')[0] && resultPwd == idAndPwd.Split(':')[1])
+                    {
+                        base.OnActionExecuting(actionContext);
+                    }
+                    else
+                    {
+                        actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized, "Invalid user id and password for web api access.");
+                    }
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.BadRequest, "Unable to get user id and password.");
+                }
+            }
+            else
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized, "Unauthorize access of web api.");
+            }
+        }
+        catch (Exception ex)
+        {
+            actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+        }
+    }
+}
